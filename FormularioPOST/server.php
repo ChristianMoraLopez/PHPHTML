59b8c867736ebbd15b898da636f48a1c7e042302
Christian Mora <christianmoralopez@hotmail.com>
2023-12-26 12:38:36 -0500

    Arrelgo de datos

--- FormularioPOST/server.php
+++ FormularioPOST/server.php
@@ -7,10 +7,10 @@ $apellido = $_POST['apellido'];
 
 //vamos a validar si el nombre y el apellido estan vacios
 if(empty($nombre) || empty($apellido)){
+    echo "<h1>El nombre y el apellido estan vacios</h1>";
+}else{
     echo "<h1>El nombre es: $nombre</h1>";
     echo "<h1>El apellido es: $apellido</h1>";
-}else{
-    echo "<h1>El nombre y el apellido no estan vacios</h1>";
     }
 
 
